22f5f3d8e43fb84e132155adf5f17416c7cdf2ad
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-18 21:09:40 +0400

    improvements and fixes

--- contract/celoinvoice.sol
+++ contract/celoinvoice.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IERC20Token {
     function transfer(address, uint256) external returns (bool);
@@ -29,43 +30,30 @@ interface IERC20Token {
     );
 }
 
-contract CeloInvoice {
+contract CeloInvoice is Ownable {
     using Counters for Counters.Counter;
 
     event InvoiceCreated(
-        uint indexed index,
+        uint256 indexed index,
         string number,
         address indexed creator,
         address indexed payer,
-        uint total
+        uint256 total
     );
     event InvoiceProccessed(
-        uint indexed index,
+        uint256 indexed index,
         address indexed creator,
         address indexed payer,
-        uint total
+        uint256 total
     );
-
-    modifier onlyOwner(uint index) {
-        require(
-            _creators[index] != address(0),
-            "You can't access this invoice"
-        );
-        _;
-    }
-
-    address owner;
 
     address internal cUsdTokenAddress =
         0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
-    Counters.Counter invoiceIndex;
-
-    constructor() {
-        owner = msg.sender;
-    }
+    Counters.Counter private invoiceIndex;
 
     enum InvoiceStatus {
+        UNDEFINED,
         PENDING,
         PAYED,
         DECLINED
@@ -76,7 +64,7 @@ contract CeloInvoice {
         string name;
         string number;
         string description;
-        uint total;
+        uint256 total;
         Item[] items;
         address payer;
         uint64 dateDue;
@@ -84,74 +72,96 @@ contract CeloInvoice {
 
     struct Item {
         string name;
-        uint price;
+        uint256 price;
     }
 
     //all generated invoices
-    mapping(uint => Invoice) allInvoices;
-
-    //map of creator to list of owned invoices;
-    mapping(address => mapping(uint => uint)) _ownedInvoices;
-
-    //map of creator to list of owned invoices;
-    mapping(address => mapping(uint => uint)) _processInvoices;
+    mapping(uint256 => Invoice) allInvoices;
 
     // tracks the number of invoices to process
-    mapping(address => uint) _receivedInvoices;
+    mapping(address => uint256) _receivedInvoices;
 
     // tracks the number of invoices created
-    mapping(address => uint) _issuedInvoices;
+    mapping(address => uint256) _issuedInvoices;
 
     // map invoice id to owner
-    mapping(uint => address) _creators;
+    mapping(uint256 => address) _creators;
 
+    modifier checkIfPayer(uint256 _index) {
+        require(
+            allInvoices[_index].payer == msg.sender,
+            "You are not the invoice's recipient"
+        );
+        _;
+    }
+
+    modifier exists(uint256 _index) {
+        require(
+            allInvoices[_index].status != InvoiceStatus.UNDEFINED,
+            "Query of nonexistent invoice"
+        );
+        _;
+    }
+
+    /**
+     * @dev allow users to create an invoice of items bought
+     * @notice input data can't be empty
+     * @param _items an array of the struct Items containing the items the invoice is based off
+     * @param _dateDue the time by which a user must pay this invoice
+     */
     function generateInvoice(
         string calldata _number,
         string calldata _name,
         string calldata _description,
         Item[] calldata _items,
-        uint _total,
+        uint256 _total,
         address _payer,
         uint64 _dateDue
     ) external {
-        uint _index = invoiceIndex.current();
-       
+        require(bytes(_number).length > 0, "Empty number");
+        require(bytes(_name).length > 0, "Empty name");
+        require(bytes(_description).length > 0, "Empty description");
+        require(_items.length > 0, "No items found for invoice");
+        require(_total > 0, "Total needs to be at least one wei");
+        require(
+            _payer != address(0),
+            "Error: Address zero is not a valid address"
+        );
+        require(
+            _dateDue > block.timestamp,
+            "Date due has to be greater than the current time"
+        );
+        uint256 _index = invoiceIndex.current();
+        invoiceIndex.increment();
+
         addInvoice(
             _index,
-              _number,
-         _name,
-         _description,
-         _items,
-         _total,
-         _payer,
-         _dateDue
+            _number,
+            _name,
+            _description,
+            _items,
+            _total,
+            _payer,
+            _dateDue
         );
-
-       // number of issued invoices of creator
-        uint creatorInvoiceCount = getOwnedInvoiceCount(msg.sender);
-
-        // number of issued invoices of payer
-        uint processInvoiceCount = getReceivedInvoiceCount(_payer);
 
         _issuedInvoices[msg.sender] += 1;
         _receivedInvoices[_payer] += 1;
 
         _creators[_index] = msg.sender;
-        _ownedInvoices[msg.sender][creatorInvoiceCount] = _index;
-
-        _processInvoices[_payer][processInvoiceCount] = _index;
-
     }
 
-    function addInvoice( uint _index, string calldata _number,
+    function addInvoice(
+        uint256 _index,
+        string calldata _number,
         string calldata _name,
         string calldata _description,
         Item[] calldata _items,
-        uint _total,
+        uint256 _total,
         address _payer,
-        uint64 _dateDue) internal {
-
-         Invoice storage newInvoice = allInvoices[_index];
+        uint64 _dateDue
+    ) internal {
+        Invoice storage newInvoice = allInvoices[_index];
         newInvoice.name = _name;
         newInvoice.number = _number;
         newInvoice.description = _description;
@@ -160,98 +170,57 @@ contract CeloInvoice {
         newInvoice.dateDue = _dateDue;
         newInvoice.status = InvoiceStatus.PENDING;
 
-        for (uint idx = 0; idx < _items.length; idx++) {
+        for (uint256 idx = 0; idx < _items.length; ) {
             newInvoice.items.push(_items[idx]);
+            // idx is already checked for every new cycle of the for loop by the for loop running condition
+            unchecked {
+                ++idx;
+            }
         }
-
-        invoiceIndex.increment();
 
         emit InvoiceCreated(_index, _number, msg.sender, _payer, _total);
     }
-    function getOwnedInvoiceCount(address _address) public view returns (uint) {
+
+    function getOwnedInvoiceCount(address _address)
+        public
+        view
+        returns (uint256)
+    {
         return _issuedInvoices[_address];
     }
 
     function getReceivedInvoiceCount(address _address)
         public
         view
-        returns (uint)
+        returns (uint256)
     {
         return _receivedInvoices[_address];
     }
 
-    function getOwnedInvoices(uint index)
-        external
+    function getInvoice(uint256 _index)
+        public
         view
-        returns (
-            uint,
-            string memory,
-            string memory,
-            string memory,
-            uint,
-            Item[] memory,
-            address,
-            InvoiceStatus,
-            uint64
-        )
+        exists(_index)
+        returns (Invoice memory)
     {
-        require(index < invoiceIndex.current(), "unknown invoice");
-        uint _invoiceIndex = _ownedInvoices[msg.sender][index];
-        Invoice memory invoice = allInvoices[_invoiceIndex];
-        
-        return (           
-            index,
-            invoice.number,
-            invoice.name,
-            invoice.description,
-            invoice.total,
-            invoice.items,
-            invoice.payer,
-            invoice.status,
-            invoice.dateDue
-        );
+        return allInvoices[_index];
     }
 
-    function getReceivedInvoice(uint _index)
+    /**
+     * @dev allow users to pay their due invoices
+     * @notice only the payer address of the invoice can make the payment
+     */
+    function makeInvoicePayment(uint256 _index)
         external
-        view
-        returns (
-            uint,
-            string memory,
-            string memory,
-            string memory,
-            uint,
-            Item[] memory,
-            address,
-            address,
-            InvoiceStatus,
-            uint64
-        )
+        exists(_index)
+        checkIfPayer(_index)
     {
-        require(_index < invoiceIndex.current(), "unknown invoice");
-        uint _invoiceIndex = _processInvoices[msg.sender][_index];
-        Invoice memory invoice = allInvoices[_invoiceIndex];
-        address _owner = _creators[_invoiceIndex];
-
-        return (
-            _index,
-            invoice.number,
-            invoice.name,
-            invoice.description,
-            invoice.total,
-            invoice.items,
-            _owner,
-            invoice.payer,
-            invoice.status,
-            invoice.dateDue
+        Invoice storage invoice = allInvoices[_index];
+        require(
+            invoice.status == InvoiceStatus.PENDING,
+            "Invoice status is not pending"
         );
-    }
-
-    function makeInvoicePayment(uint _index) external {
-        uint _invoiceIndex = _processInvoices[msg.sender][_index];
         address creator = _creators[_index];
-
-        Invoice storage invoice = allInvoices[_invoiceIndex];
         invoice.status = InvoiceStatus.PAYED;
 
         require(
@@ -264,19 +233,34 @@ contract CeloInvoice {
         );
     }
 
-    function declineInvoice(uint _index) external {
-        uint _invoiceIndex = _processInvoices[msg.sender][_index];
-        Invoice storage invoice = allInvoices[_invoiceIndex];
+    /**
+     * @dev allow users to decline an invoice in case of certain situations such as fake invoices or scams
+     * @notice only the payer address can decline an invoice
+     */
+    function declineInvoice(uint256 _index)
+        external
+        exists(_index)
+        checkIfPayer(_index)
+    {
+        Invoice storage invoice = allInvoices[_index];
 
         invoice.status = InvoiceStatus.DECLINED;
     }
 
-    function deleteInvoice(uint _index) external {
-        uint _invoiceIndex = _ownedInvoices[msg.sender][_index];
-
+    /**
+     * @dev allow invoices' creators to delete an invoice
+     * @dev invoice's data will be removed
+     * @notice only the invoice's creator can delete the invoice
+     */
+    function deleteInvoice(uint256 _index) external exists(_index) {
+        require(
+            _creators[_index] == msg.sender,
+            "You are not the invoice's creator"
+        );
         _issuedInvoices[msg.sender] -= 1;
-
-        delete allInvoices[_invoiceIndex];
-        delete _ownedInvoices[msg.sender][_index];
+        uint256 invoicesLength = invoiceIndex.current() - 1;
+        invoiceIndex.decrement();
+        allInvoices[_index] = allInvoices[invoicesLength];
+        delete allInvoices[invoicesLength];
     }
 }
